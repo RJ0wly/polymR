bfe544e9a0886552b150a6fdee343c7243d744b4
RJ0wly <56268620+RJ0wly@users.noreply.github.com>
2021-07-07 12:01:58 +0200

    Add files via upload

---
+++ R/poly.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(stringr)
+library(stringi)
+
+#' polynomial_features
+#' 
+#' Generate a new feature matrix consisting of all polynomial 
+#' combinations of the features with degree less than or equal to the specified 
+#' degree.
+#' @usage polynomial_features(dataframe,degree)
+#' @param df Dataframe, containing the features of interest.
+#' @param degree Integer, the degree of the polynomial.
+#' @examples 
+#' val1 = rnorm(50)
+#' val2 = rnorm(50)
+#' df = as.data.frame(cbind(val1,val2))
+#' df_poly = polynomialFeatures(df,2)
+#' @export
+polynomial_features <- function(df,degree){
+tmp =  vector()
+cpt = 1
+len = degree*length(df)
+while(cpt < len){
+  for(j in colnames(t)){
+    for(i in seq_along(1:degree)){
+      tmp[[cpt]] = paste0('poly(',j,',',degree,',raw=TRUE)[,',i,']')
+      cpt = cpt + 1
+    }
+  }
+}
+
+polym = stri_flatten(tmp,collapse="+")
+formula <- as.formula(paste(' ~ .^',degree,'+',paste0(polym),collapse="+"))
+tmp_pol = model.frame(formula, data=df) 
+tmp_pol_poly = tmp_pol[,grepl("poly",colnames(tmp_pol))]
+
+regex_filter =  "poly\\(([a-zA-Z0-9_]*),\\s*\\d*\\s*,\\s*raw\\s*=\\s*TRUE\\)\\[\\s*,\\s*(\\d+)\\]"
+for(i in colnames(tmp_pol_poly)){
+  string_of_interest = str_match(string = i, pattern = regex_filter)
+  names(tmp_pol_poly)[names(tmp_pol_poly) == i] <- paste0(paste0(string_of_interest[2],"^"),
+                                                          string_of_interest[3])
+}
+tmp_pol = cbind(tmp_pol,tmp_pol_poly)
+tmp_pol = dplyr::select(tmp_pol, -c(colnames(tmp_pol[,grepl("poly",colnames(tmp_pol))])))
+
+formula =  as.formula(paste('~ .^',degree))
+tmp_inter = model.matrix(formula, data=df)
+tmp_inter = as.data.frame(tmp_inter)
+tmp =  cbind(tmp_inter,tmp_pol)
+}
